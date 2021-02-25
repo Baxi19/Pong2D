8329c6ee6bcb9c6af00f148a68b454a74973378e
Baxi19 <randald1991@gmail.com>
2021-02-24 23:00:29 -0600

    :fire: some collisions added :octocat:

---
+++ Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour
+{
+    public Transform paddle;
+    bool gameStarted = false;
+    public Rigidbody2D rigidbodyBall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!gameStarted){
+            float posDif = paddle.position.x - transform.position.x; 
+            transform.position = new Vector3(paddle.position.x - posDif, paddle.position.y, paddle.position.z);    
+
+            //if user click
+            if(Input.GetMouseButtonDown(0)){
+                rigidbodyBall.velocity = new Vector2(8,8);
+                gameStarted = true;
+            }
+        }
+        
+        
+    }
+}
---
+++ Assets/Scripts/DeadZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class DeadZone : MonoBehaviour
+{
+    private void onCollisionEnter2D(Collision2D collision){
+        Debug.Log("Collision");
+    }
+
+    private void onTriggerEnter2D(Collider2D collision){
+        Debug.Log("Trigger");
+    }
+}
