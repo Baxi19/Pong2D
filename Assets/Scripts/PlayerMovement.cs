cb6680457cbc4fb144a82baeb92930bbc9a37d02
Baxi19 <randald1991@gmail.com>
2021-02-24 19:15:43 -0600

    :octocat: Pong game in Android

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Input.mousePosition);
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
+    }
+}
